ac2aaf7fdbc57e2a9dbf7eb306b21b857f78aff6
David Kocman <kocmadav@fit.cvut.cz>
2023-10-13 13:03:08 +0200

    Updated solution

---
+++ ukol1/src/fizzbuzz.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env/ruby
+
+left_num = nil
+left_num == nil ? left_num = 1 : left_num = left_num
+right_num = 20
+right_num == nil ? right_num = 100 : right_num = right_num
+
+is_divisible = false
+
+divisibles = [
+    [3, "Fizz "], 
+    [5, "Buzz "],
+    [7, "Bish "],
+    [4, "Bash "],
+    [6, "Bosh "]
+]
+
+divisibles.length < 2 ? divisibles = [[3, "Fizz "], [5, "Buzz "]] : divisibles = divisibles
+
+left_num.upto(right_num) do |i|
+
+    divisibles.each do |(num, text)|
+        if i % num == 0
+            print text
+            is_divisible = true
+        end
+    end
+
+    print i unless is_divisible
+    print "," unless i == right_num
+    is_divisible = false
+end
