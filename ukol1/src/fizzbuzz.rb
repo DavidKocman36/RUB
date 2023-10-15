2af95b4373edc502676847550dfc3772418e380e
David Kocman <kocmadav@fit.cvut.cz>
2023-10-15 12:30:49 +0200

    Some tweaks to fizzbuzz

--- ukol1/src/fizzbuzz.rb
+++ ukol1/src/fizzbuzz.rb
@@ -1,32 +1,34 @@
 #!/usr/bin/env/ruby
 
+# naplneni default hodnotama, pokud nejsou specifikovany
 left_num = nil
-left_num == nil ? left_num = 1 : left_num = left_num
+left_num ||= 1
 right_num = nil
-right_num == nil ? right_num = 100 : right_num = right_num
+right_num ||= 100
 
 is_divisible = false
 
 divisibles = [
-    [3, "Fizz "], 
-    [5, "Buzz "],
-    [7, "Bish "],
-    [4, "Bash "],
-    [6, "Bosh "]
+    [3, "Fizz"], 
+    [5, "Buzz"],
+    [7, "Bish"],
+    [4, "Bash"],
+    [6, "Bosh"]
 ]
 
-divisibles.length < 2 ? divisibles = [[3, "Fizz "], [5, "Buzz "]] : divisibles = divisibles
+divisibles.length < 2 ? divisibles = [[3, "Fizz"], [5, "Buzz"]] : divisibles = divisibles
 
 left_num.upto(right_num) do |i|
 
     divisibles.each do |(num, text)|
         if i % num == 0
+            print " " if is_divisible
             print text
             is_divisible = true
         end
     end
 
     print i unless is_divisible
-    print "," unless i == right_num
+    print ", " unless i == right_num
     is_divisible = false
 end
