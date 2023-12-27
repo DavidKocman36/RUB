5dc36838e2d21041d7fb7a0f3c645ebbac0279a5
David Kocman <kocmadav@fit.cvut.cz>
2023-12-27 01:05:25 +0100

    tests

--- blog/blog/test/controllers/articles_controller_test.rb
+++ blog/blog/test/controllers/articles_controller_test.rb
@@ -4,4 +4,44 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "get index" do
+    get articles_url
+    assert_response :success
+  end
+
+  test "create article redirect" do
+    assert_difference("Article.count") do
+      post articles_url, params: { article: { body: "Test body is here heloooo", title: "Hello everybody", prenex: "a new prenex" } }
+    end
+  
+    assert_redirected_to articles_path
+  end
+
+  test "update article" do
+    article = articles(:one)
+
+    patch article_url(article), params: { article: { title: "New title", body: "Test body is here heloooo", prenex: "a new prenex" } }
+
+    assert_redirected_to article_path(article)
+    
+    article.reload
+    assert_equal "New title", article.title
+  end
+
+  test "delete article" do
+    article = articles(:one)
+    assert_difference("Article.count", -1) do
+      delete article_url(article)
+    end
+
+    assert_redirected_to articles_path
+  end
+
+  test "show article" do
+    article = articles(:one)
+    get article_url(article)
+    assert_response :success
+  end
+
 end
